f154fb177f34d102735879438fcb31bf64a2bf02
melissa billard <billardmelissa1@gmail.com>
2025-06-03 09:58:11 +0200

    add react Styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  components: "src/components/**/*.jsx", // adapte si ton dossier n'est pas src/components
+  exampleMode: "expand", // les exemples de code seront visibles par défaut
+  usageMode: "expand", // affiche la section "Props" automatiquement
+};
